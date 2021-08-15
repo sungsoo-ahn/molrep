85006f390198b9a33da00fbb97ecad5c721648d0
Sungsoo <sungsahn0215@gmail.com>
2021-08-15 23:18:00 +0400

    tmp

--- script/submit.sh
+++ script/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=molrep
+#SBATCH --job-name=frag
 #SBATCH --partition=mbzuai
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --output=/nfs/projects/mbzuai/peterahn/workspace/mol-hrl/resource/log/job_%j.log
+#SBATCH --output=/nfs/projects/mbzuai/peterahn/workspace/molrep/resource/log/job_%j.log
 #SBATCH -N 1
 #SBATCH -G 1
 #SBATCH --cpus-per-task=8
@@ -12,6 +12,6 @@
 srun \
   --container-image=sungsahn0215/mol-hrl:latest \
   --no-container-mount-home \
-  --container-mounts="/nfs/projects/mbzuai/peterahn/workspace/mol-hrl:/mol-hrl" \
-  --container-workdir="/mol-hrl/src" \
+  --container-mounts="/nfs/projects/mbzuai/peterahn/workspace/molrep:/molrep" \
+  --container-workdir="/molrep/src" \
   bash ../script/${1}
